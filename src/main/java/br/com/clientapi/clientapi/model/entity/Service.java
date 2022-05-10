f990e6da3fe966bcd5e98c3fa1a0028a4ea5bbc8
mateusssilva <mateussousa54321@gmail.com>
2022-05-09 22:23:50 -0300

    Commit inicial

---
+++ src/main/java/br/com/clientapi/clientapi/model/entity/Service.java
@@ -0,0 +1,25 @@
+package br.com.clientapi.clientapi.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_service")
+    private Integer IdService;
+
+    @Column(name = "description_service", nullable = false, length = 500)
+    private String descriptionService;
+
+    @Column(name = "value_service", nullable = false)
+    private BigDecimal valueService;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client")
+    private Client client;
+}
